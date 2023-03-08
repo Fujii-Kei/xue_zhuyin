ee97315f1e3cf1d949972dade6eab74ffe32aa03
fujiikei <azyuyu@gmail.com>
2023-03-08 19:02:27 +0900

    bookmark_model_added #8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_053601) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_092735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_053601) do
     t.string "answer_audio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "question_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_bookmarks_on_question_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "question_answers", force: :cascade do |t|
@@ -47,6 +56,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_053601) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "bookmarks", "questions"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "question_answers", "answers"
   add_foreign_key "question_answers", "questions"
 end
