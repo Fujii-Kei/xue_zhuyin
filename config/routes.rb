c983dd24363feccd6f36d0e025819dfb6ed2dc82
fujiikei <azyuyu@gmail.com>
2023-03-22 22:15:20 +0900

    fix_categories_and_questions_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'question/index'
   root 'top_pages#top'
+  resources :categories, only: %i[index show] do
+  resources :questions, only: %i[index]
+  end
   resources :users, only: %i[new create] 
   get 'login', to: 'user_sessions#new', as: 'login'
   post 'login', to: 'user_sessions#create'
