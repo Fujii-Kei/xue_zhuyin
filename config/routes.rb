a0f0e3781aceaf932d0357c308bfb0a06cb0e5ae
fujiikei <azyuyu@gmail.com>
2023-04-06 14:56:30 +0900

    results_show_routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :questions, only: %i[index]
   end
   resources :users, only: %i[new create] 
+  resources :results, only: %i[show]
   get 'login', to: 'user_sessions#new', as: 'login'
   post 'login', to: 'user_sessions#create'
   post 'logout', to: 'user_sessions#destroy', as: 'logout'
