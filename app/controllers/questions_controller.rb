b82e7855b57651646a74448beb648846219ed13a
fujiikei <azyuyu@gmail.com>
2023-04-10 14:14:52 +0900

    question_form_fixed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  def index
+  def show
     @questions = Question.all
   end
 end
--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,21 +2,23 @@ class ResultsController < ApplicationController
   def index
   end
 
-  def show
-    correct_count = 0
-    incorrect_count = 0
-
-    questions.each do |question|
-      user_answer = answers[question[:id].to_s]
-      if user_answer.to_i == question[:answer]
-        correct_count += 1
-      else
-        incorrect_count += 1
-      end
+  def create
+    @result = Result.new(result_params)
+    
+    if @result.save
+      redirect_to category_question_result_path(@result)
+    else
+      render 'questions/index'
     end
-
-    # カウントした結果をビューに渡す
-    @correct_answers = correct_count
-    @incorrect_answers = incorrect_count
   end
+
+  def show
+    @result = Result.find(params[:id])
+  end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def result_params
+      params.require(:result).permit(:score)
+    end
 end
