94b1c3a554ac506a893e75a1d22cfdcf36d37b08
fujiikei <azyuyu@gmail.com>
2023-06-06 11:06:24 +0900

    routes_fixed

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,10 +6,20 @@ class BookmarksController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
     current_user.bookmark(@question)
+    render turbo_stream: turbo_stream.replace(
+      'bookmark-button',
+      partial: 'results/bookmark_button',
+      locals: { question: @question }
+    )
   end
 
   def destroy
     @question = Question.find(params[:question_id])
     current_user.unbookmark(@question)
+    render turbo_stream: turbo_stream.replace(
+      'bookmark-button',
+      partial: 'results/bookmark_button',
+      locals: { question: @question }
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,14 @@ Rails.application.routes.draw do
       root to: "user_answers#index"
     end
   root 'top_pages#top'
+  resources :bookmarks, only: %i[index]
   resources :categories, only: %i[index show] do
     resource :question, only: %i[show] do
+      resource :bookmark, only: %i[create destroy]
       resources :results, only: %i[index show create]
     end
   end
   resources :users, only: %i[new create]
-  resources :bookmarks, only: %i[index create destroy]
   resource :profile, only: %i[edit show update]
   get 'login', to: 'user_sessions#new', as: 'login'
   post 'login', to: 'user_sessions#create'
