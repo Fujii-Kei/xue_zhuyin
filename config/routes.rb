bcdc38e89764df4b8195e9a98e27f7ade5cef1eb
fujiikei <azyuyu@gmail.com>
2023-08-15 21:13:03 +0900

    check_admin

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   end
   resources :users, only: %i[new create]
   namespace :admin do
+    root 'dashboards#index'
+    get 'login', to: 'user_sessions#new', as: 'login'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy', as: 'logout'
     resources :users
   end
   resource :profile, only: %i[edit show update]
