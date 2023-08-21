aff80a318bfddc203ba298ce32ed9336ad34eb2a
fujiikei <azyuyu@gmail.com>
2023-08-22 01:03:35 +0900

    admin_users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,20 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: 'User was successfully created.'
+    else
+      flash.now[:alert] = 'User was failed to create.'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def edit; end
 
   def update
@@ -28,6 +42,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :role)
+    params.require(:user).permit(:name, :email, :role, :password, :password_confirmation, :avatar, :biography, :avatar_cache)
   end
 end
